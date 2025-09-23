692e02ff8648afa9f404ab00600f2a28bcf71644
prasanrahulritgit <prasanrahulritgit@gmail.com>
2025-09-23 10:11:16 +0530

    Pull the new changes

--- src/components/Auth/Landingpage.jsx
+++ src/components/Auth/Landingpage.jsx
@@ -19,10 +19,9 @@ import {
 import rutomatrix from "../../assets/RUTOMATRIX-bg1.png";
 import sl1 from "../../assets/Sl1.png";
 import sl2 from "../../assets/Sl2.png";
-import sl3 from "../../assets/bg3_.png";
+import sl3 from "../../assets/Sl3.png";
 import sl4 from "../../assets/Sl4.png";
 import sl5 from "../../assets/Sl5.jpg";
-// import sl5 from "../../assets/Sl5.png";
 import logo from "../../assets/RutoMatrix_Nonbackground.png";
 import tes_logo from "../../assets/tessolve.png";
 import view1 from "../../assets/images/Top1.png";
